9fe86f055359765568fc29ebad4696d939f6ad85
VuXuanHoangLam <blackkingofdead@gmail.com>
2024-10-27 23:11:09 +0700

    2nd commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 using WebBanHang.Models;
+using WebBanHang.ViewModels;
 using X.PagedList;
 
 namespace WebBanHang.Controllers
@@ -21,11 +22,17 @@ namespace WebBanHang.Controllers
         {
             int pageSize = 8;
             int pageNumber = page == null || page < 0 ? 1 : page.Value;
-            var listHD = db.DanhMucHoatDongs.AsNoTracking().OrderBy(x=>x.TenHd);
+            var listHD = db.DanhMucHoatDongs.AsNoTracking().OrderBy(x => x.TenHd);
             PagedList<DanhMucHoatDong> lst = new PagedList<DanhMucHoatDong>(listHD, pageNumber, pageSize);
             return View(lst);
         }
-
+        public IActionResult ChiTietHoatDong(string maHD)
+        {
+            var hoatDong = db.DanhMucHoatDongs.SingleOrDefault(x => x.MaHd == maHD);
+            var anhHoatDong = db.AnhHds.Where(x => x.MaHd == maHD).ToList();
+            ViewBag.anhHoatDong = anhHoatDong;
+            return View(hoatDong);
+        } 
         public IActionResult HoatDongTheoLoai(string maLoaiHD, int? page)
         {
             int pageSize = 8;
@@ -34,6 +41,19 @@ namespace WebBanHang.Controllers
             PagedList<DanhMucHoatDong> lst = new PagedList<DanhMucHoatDong>(listHD, pageNumber, pageSize);
             ViewBag.maLoaiHD = maLoaiHD;
             return View(lst);
+        }
+        public IActionResult ActivitiesDetail(string maHD)
+        {
+            var hoatDong = db.DanhMucHoatDongs.SingleOrDefault(x => x.MaHd == maHD);
+            var chiTiet = db.ChiTietHoatDongs.SingleOrDefault(x => x.MaHd == maHD);
+            var anhHoatDong = db.AnhHds.Where(x => x.MaHd == maHD).ToList();
+            var actDetViewModel = new ActivitiesDetailViewModel 
+            { 
+                anhHds = anhHoatDong, 
+                danhMucHoatDong = hoatDong, 
+                chiTietHoatDong = chiTiet 
+            };
+            return View(actDetViewModel);
         }
         public IActionResult Privacy()
         {
---
+++ ViewModels/ActivitiesDetailViewModel.cs
@@ -0,0 +1,10 @@
+﻿using WebBanHang.Models;
+namespace WebBanHang.ViewModels
+{
+    public class ActivitiesDetailViewModel
+    {
+        public DanhMucHoatDong danhMucHoatDong { get; set; }
+        public ChiTietHoatDong chiTietHoatDong { get; set; }
+        public List<AnhHd> anhHds { get; set; }
+    }
+}
